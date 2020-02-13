b003d446f3f52dd18d0fcb46a28e8baa094b6fdc
alumno <alumno@utnfra.com.ar>
2020-02-13 20:22:37 -0300

    Done AgilidadAritmetica 1

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,45 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var minNumeber = 1;
+var maxNumber = 10;
+var numero1 = 0;
+var numero2 = 0;
+var operadores = ['+', '-', '/', '*'];
+var operador = '';
 function comenzar()
 {
+    numero1 = Math.floor(Math.random() * (maxNumber - minNumeber + 1) + minNumeber);
+    numero2 = Math.floor(Math.random() * (maxNumber - minNumeber + 1) + minNumeber);
+    operador = operadores[Math.floor(Math.random() * 4)];
 
-	
+    switch (operador) {
+        case '+':
+            respuesta = numero1 + numero2;
+            break;
+        case '-':
+            respuesta = numero1 - numero2;
+            break;
+        case '*':
+            respuesta = numero1 * numero2;
+            break;
+        case '/':
+            respuesta = numero1 / numero2;
+            break;
+    }
 
+    document.getElementById('PrimerNumero').value = numero1;
+    document.getElementById('SegundoNumero').value = numero2;
+    document.getElementById('Operador').value = operador;
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    var respuestaUsuario = parseFloat(document.getElementById('Respuesta').value);
+    var mensaje = 'Respuesta Incorrecta';
 
+    if (respuesta == respuestaUsuario) {
+        mensaje = 'Respuesta Correcta';
+    }
+    
+    alert(mensaje);
 }//FIN DE LA FUNCIÓN
