b73ed0e216edee9cf51b24e3a336cacf7e4d0720
alumno <alumno@utnfra.com.ar>
2020-02-13 19:00:55 -0300

    Done PiedraPapelTijera 1

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,25 +5,64 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var minNumeber = 1;
+var maxNumber = 3;
 
 function comenzar()
 {
-	
-
-
+    eleccionMaquina = Math.floor(Math.random() * (maxNumber - minNumeber + 1) + minNumeber);
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	var mensaje = '';
+
+    switch (eleccionMaquina) {
+        case 2:
+            mensaje = 'Perdiste';
+            break;
+        case 3:
+            mensaje = 'Ganaste';
+            break;
+        default:
+            mensaje = 'Empate';
+    }
+
+    alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	var mensaje = '';
 
+    switch (eleccionMaquina) {
+        case 1:
+            mensaje = 'Ganaste';
+            break;
+        case 3:
+            mensaje = 'Perdiste';
+            break;
+        default:
+            mensaje = 'Empate';
+    }
+
+    alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	var mensaje = '';
+
+    switch (eleccionMaquina) {
+        case 1:
+            mensaje = 'Perdiste';
+            break;
+        case 2:
+            mensaje = 'Ganaste';
+            break;
+        default:
+            mensaje = 'Empate';
+    }
+
+    alert(mensaje);	
 
 }//FIN DE LA FUNCIÓN
