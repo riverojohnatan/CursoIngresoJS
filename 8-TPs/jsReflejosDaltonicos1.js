b4ad271062ed445be61432a46170f8d69ba476bc
alumno <alumno@utnfra.com.ar>
2020-02-13 21:36:33 -0300

    Done ReflejosDaltonicos 1

--- 8-TPs/jsReflejosDaltonicos1.js
+++ 8-TPs/jsReflejosDaltonicos1.js
@@ -4,17 +4,22 @@ mostrara el texto de un color entre los 6 posibles
  para que el jugador sepa que botón tocar .Al tocar 
  el botón correcto se informara cuanto tiempo tardo.
 */
-var ColorSecreto;
+var colorSecreto;
 var tiempoInicio;
+var colores = ["azul", "amarillo", "marron", "verde", "celeste", "rojo"];
 function comenzar()
 {
+    colorSecreto = colores[Math.floor(Math.random() * 6)];
+    document.getElementById('ColorElejido').value = colorSecreto;
+    tiempoInicio = 0;
 
-	
-
+    setInterval(()=>tiempoInicio++, 1000);
 }//FIN DE LA FUNCIÓN
 function Responder(colorParametro)
 {
-	
+	if (colorSecreto === colorParametro) {
+        alert(`Tardaste ${tiempoInicio} segundos`);
+    }
 	
 
 
